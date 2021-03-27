339f719d9cf2cf3cee128e9d14188411d91dad3d
wbrijesh <brijesh@wawdhane.com>
2021-03-27 05:54:57 +0530

    Show lecture posts to logged in users

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
-import Head from 'next/head'
 import Navbar from '../components/Navbar/Navbar'
 import { useUser } from '@auth0/nextjs-auth0';
-import React, { useState } from 'react';
+import Lecture_cards from '../components/Lecture_cards';
 
 
 export default function Home() {
@@ -11,38 +10,12 @@ export default function Home() {
   if (error) return <div>{error.message}</div>;
 
   if (user) {
-    let userName = user.name.substring(0, user.name.indexOf("@"));
     return (
       <div className="bg-gray-1">
 
         <Navbar />
         Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
-
-        <div className="container">
-          <div>
-            <div className="columns">
-                <div className="column has-text-centered">
-                  <h1 className="title" style={{textTransform: 'capitalize'}}>Welcome {userName}</h1><br />
-                </div>
-            </div>
-            <div id="app" className="row columns is-multiline">
-                <div className="column is-4">
-
-                  <div className="card large">
-                    <div className="card-content">
-                      <div className="media">
-                        <div className="media-content">
-                          <p className="title is-4 no-padding">Lead Developer</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                
-                </div>
-                
-              </div>
-          </div>
-        </div>
+        <Lecture_cards />
 
       </div>
     );
