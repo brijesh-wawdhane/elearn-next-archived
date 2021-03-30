a741545d244700b48d221f65608307790ab522d7
wbrijesh <brijesh@wawdhane.com>
2021-03-30 21:36:55 +0530

    restart

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,55 @@
-import '../styles/globals.css';
-import React from 'react';
-import { UserProvider } from '@auth0/nextjs-auth0';
-import '../configureAmplify'
+import "../styles/globals.css";
+import "../configureAmplify";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { Auth, Hub } from "aws-amplify";
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
+  const [signedInUser, setSignedInUser] = useState(false);
+
+  useEffect(() => {
+    authListener();
+  });
+  async function authListener() {
+    Hub.listen("auth", (data) => {
+      switch (data.payload.event) {
+        case "signIn":
+          return setSignedInUser(true);
+        case "signOut":
+          return setSignedInUser(false);
+      }
+    });
+    try {
+      await Auth.currentAuthenticatedUser();
+      setSignedInUser(true);
+    } catch (err) {}
+  }
+
   return (
-    <UserProvider>
-      <div className="">
+    <div>
+      <nav className="p-6 border-b border-gray-300">
+        <Link href="/">
+          <span className="mr-6 cursor-pointer">Home</span>
+        </Link>
+        <Link href="/create-post">
+          <span className="mr-6 cursor-pointer">Create Post</span>
+        </Link>
+
+        {signedInUser && (
+          <Link href="/my-posts">
+            <span className="mr-6 cursor-pointer">My Posts</span>
+          </Link>
+        )}
+
+        <Link href="/profile">
+          <span className="mr-6 cursor-pointer">Profile</span>
+        </Link>
+      </nav>
+      <div className="py-8 px-16">
         <Component {...pageProps} />
       </div>
-    </UserProvider>
+    </div>
   );
 }
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,30 @@
-import Navbar from '../components/Navbar/Navbar'
-import { useUser } from '@auth0/nextjs-auth0';
-import Lecture_cards from '../components/Lecture_cards';
-
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { API } from "aws-amplify";
+import { listPosts } from "../graphql/queries";
 
 export default function Home() {
-  const { user, error, isLoading } = useUser();
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
-
-  if (user) {
-    return (
-      <div className="bg-gray-1">
-
-        <Navbar />
-        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
-        <Lecture_cards />
-
-      </div>
-    );
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+  async function fetchPosts() {
+    const postData = await API.graphql({
+      query: listPosts,
+    });
+    setPosts(postData.data.listPosts.items);
   }
-
-  return <><Navbar /><a href="/api/auth/login">Login</a></>;
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold tracking-wide mt-6 mb-2">Posts</h1>
+      {posts.map((post, index) => (
+        <Link key={index} href={`/posts/${post.id}`}>
+          <div className="cursor-pointer border-b border-gray-300	mt-8 pb-4">
+            <h2 className="text-xl font-semibold">{post.title}</h2>
+            <p className="text-gray-500 mt-2">Author: {post.username}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
